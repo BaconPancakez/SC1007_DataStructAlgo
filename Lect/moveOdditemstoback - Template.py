5278794af243439634c0bb2ce6a1db9fc3a41d57
BaconPancakez <sengqiming15@gmail.com>
2025-03-01 00:46:04 +0800

    update order

---
+++ Lect/moveOdditemstoback - Template.py
@@ -0,0 +1,231 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.size = 0
+
+    def findNode(self, index):
+        if index < 0 or index >= self.size:
+            raise ValueError("Invalid position")
+        if self.head is None:
+            raise ValueError("List is empty")
+
+        cur = self.head
+        while index > 0:
+            cur = cur.next
+            index -= 1
+        return cur
+
+    def insertNode(self, data, index):
+        if index < 0 or index > self.size:
+            raise ValueError("Invalid position")
+
+        new_node = Node(data)
+
+        if index == 0:
+            new_node.next = self.head
+            self.head = new_node
+            self.size += 1
+            return True
+
+        prev_node = self.findNode(index - 1)
+        if prev_node is not None:
+            new_node.next = prev_node.next
+            prev_node.next = new_node
+            self.size += 1
+            return True
+        return False
+
+    def removeNode(self, index):
+        if index < 0 or index >= self.size:
+            raise ValueError("Invalid position")
+
+        if self.head is None:
+            return False
+
+        if index == 0:
+            cur = self.head
+            self.head = cur.next
+            self.size -= 1
+            return True
+
+        pre = self.findNode(index - 1)
+        if pre is not None and pre.next is not None:
+            cur = pre.next
+            pre.next = cur.next
+            self.size -= 1
+            return True
+        return False
+
+    def printList(self):
+        cur = self.head
+        if cur is None:
+            print("Empty")
+            return
+        while cur is not None:
+            print(cur.data, end=" -> ")
+            cur = cur.next
+        print("None")
+
+
+def moveOdditemstoback(head):
+ # Write your code here #
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.size = 0
+
+    def findNode(self, index):
+        if index < 0 or index >= self.size:
+            raise ValueError("Invalid position")
+        if self.head is None:
+            raise ValueError("List is empty")
+
+        cur = self.head
+        while index > 0:
+            cur = cur.next
+            index -= 1
+        return cur
+
+    def insertNode(self, data, index):
+        if index < 0 or index > self.size:
+            raise ValueError("Invalid position")
+
+        new_node = Node(data)
+
+        if index == 0:
+            new_node.next = self.head
+            self.head = new_node
+            self.size += 1
+            return True
+
+        prev_node = self.findNode(index - 1)
+        if prev_node is not None:
+            new_node.next = prev_node.next
+            prev_node.next = new_node
+            self.size += 1
+            return True
+        return False
+
+    def removeNode(self, index):
+        if index < 0 or index >= self.size:
+            raise ValueError("Invalid position")
+
+        if self.head is None:
+            return False
+
+        if index == 0:
+            cur = self.head
+            self.head = cur.next
+            self.size -= 1
+            return True
+
+        pre = self.findNode(index - 1)
+        if pre is not None and pre.next is not None:
+            cur = pre.next
+            pre.next = cur.next
+            self.size -= 1
+            return True
+        return False
+
+    def printList(self):
+        cur = self.head
+        if cur is None:
+            print("Empty")
+            return
+        while cur is not None:
+            print(cur.data, end=" -> ")
+            cur = cur.next
+        print("None")
+
+
+def moveOdditemstoback(head):
+    if head is None or head.next is None:
+        return head
+
+    even_head = even_tail = None
+    odd_head = odd_tail = None
+
+    curr = head
+
+    while curr:
+        next_node = curr.next
+        curr.next = None
+
+        if curr.data % 2 == 0:
+            if even_head is None:  # First Node
+                even_head = even_tail = curr
+            else:  # Subsequent Node
+                even_tail.next = curr
+                even_tail = curr
+        else:
+            if odd_head is None:  # First Node
+                odd_head = odd_tail = curr
+            else:  # Subsequent Node
+                odd_tail.next = curr
+                odd_tail = curr
+
+        curr = next_node
+
+    if not even_head:
+        return odd_head
+
+    even_tail.next = odd_head
+    return even_head
+
+
+# Write your code here #
+if __name__ == "__main__":
+    linked_list = LinkedList()
+
+    print("Enter a list of numbers, terminated by any non-digit character: ", end="")
+    input_string = input()
+    numbers = input_string.split()
+
+    counter = 0
+    for num in numbers:
+        try:
+            linked_list.insertNode(int(num), counter)
+            counter += 1
+        except ValueError:
+            break
+
+    print("\nBefore:", end=" ")
+    linked_list.printList()
+    linked_list.head = moveOdditemstoback(linked_list.head)
+    print("After:", end=" ")
+    linked_list.printList()
+
+if __name__ == "__main__":
+    linked_list = LinkedList()
+
+    print("Enter a list of numbers, terminated by any non-digit character: ", end="")
+    input_string = input()
+    numbers = input_string.split()
+
+    counter = 0
+    for num in numbers:
+        try:
+            linked_list.insertNode(int(num), counter)
+            counter += 1
+        except ValueError:
+            break
+
+    print("\nBefore:", end=" ")
+    linked_list.printList()
+    linked_list.head = moveOdditemstoback(linked_list.head)
+    print("After:", end=" ")
+    linked_list.printList()
